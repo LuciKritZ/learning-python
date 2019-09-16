ed5500f5aad3cb15354ca5ebf71748029fc6ae77
LuciKritZ <krishals.001@gmail.com>
2019-09-16 12:08:50 +0530

    Interface

--- Programs/interface/interface.py
+++ Programs/interface/interface.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod, ABC
+# An interface class where all the methods in the class are abstract methods.
 class BMW(ABC):
     def __init__(self, make, model, year):
         self.make = make
@@ -6,11 +7,13 @@ class BMW(ABC):
         self.year = year
     
     # Methods:
+    @abstractmethod
     def start(self):
-        print("Starting the "+self.model)
+        pass
     
+    @abstractmethod
     def stop(self):
-        print("Stopping the "+self.model)
+        pass
 
     @abstractmethod
     def drive(self):
@@ -36,35 +39,6 @@ class Audi(ABC):
     def drive(self):
         # for passing the method without doing the implementation.
         pass
-
-class ThreeSeriesAudi(Audi):
-    def __init__(self, cruiseControlEnabled, make, model, year):
-        # BMW.__init__(self, make, model, year)
-        # We can use the super keyword in Python, which will by default choose the super or the parent
-        # class instead of writing the parent class name and then we will not include self.
-        super().__init__(make, model, year)
-        self.cruiseControlEnabled = cruiseControlEnabled
-    
-    # In addition to this, the child class can also define it's own functions:
-    def display(self):
-        print("The cruise control is "+str(self.cruiseControlEnabled))
-    
-    # Method Overriding
-    def start(self):
-        # If we want to invoke the overridden function of the parent class, it can be done using the
-        # super keyword.
-        super().start()
-        print("Started!")
-    
-    def stop(self):
-        # If we want to invoke the overridden function of the parent class, it can be done using the
-        # super keyword.
-        super().stop()
-        print("Stopped!")
-    
-    # Defining the abstract class from the parent class
-    def drive(self):
-        print("Three series is being driven")
     
 # To inherit the class, we include them in the brackets just as in a function in python.
 class ThreeSeries(BMW):
@@ -86,6 +60,10 @@ class ThreeSeries(BMW):
         super().start()
         print("Started!")
     
+    def stop(self):
+        super().stop()
+        print("Stopped")
+    
     # Defining the abstract class from the parent class
     def drive(self):
         print("Three series is being driven")
@@ -98,6 +76,17 @@ class FiveSeries(BMW):
     # Defining the abstract class from the parent class
     def drive(self):
         print("Five series is being driven.")
+
+    def start(self):
+        # If we want to invoke the overridden function of the parent class, it can be done using the
+        # super keyword.
+        super().start()
+        print("Started!")
+    
+    def stop(self):
+        super().stop()
+        print("Stopped")
+    
 
 # Creating the object of the child class:
 threeSeries = ThreeSeries(True, "BMW", "328i", 2018)
@@ -126,16 +115,3 @@ print(fiveSeries.make)
 print(fiveSeries.model)
 print(fiveSeries.year)
 fiveSeries.stop()
-
-# Let's see how we can inherit the methods from the parent class to the child class.
-
-# Audi
-threeSeriesAudi = ThreeSeriesAudi(True, "BMW", "328i", 2018)
-threeSeriesAudi.start()
-threeSeriesAudi.drive()
-print(threeSeriesAudi.cruiseControlEnabled)
-print(threeSeriesAudi.make)
-print(threeSeriesAudi.model)
-print(threeSeriesAudi.year)
-threeSeriesAudi.display()
-threeSeriesAudi.stop()
